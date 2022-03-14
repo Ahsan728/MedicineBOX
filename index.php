a3c1908a667944c434fcf1be6a5efeaf85548605
AhsanSuny <34878134+Ahsan728@users.noreply.github.com>
2022-03-14 19:11:52 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+if (!isset($_SESSION['Admin-name'])) {
+  header("location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patients</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="icons/atte1.jpg">
+
+    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/Users.css">
+    <script>
+      $(window).on("load resize ", function() {
+        var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+        $('.tbl-header').css({'padding-right':scrollWidth});
+    }).resize();
+    </script>
+</head>
+<body>
+<?php include'header.php'; ?> 
+<main>
+<section>
+ 
+  <!--User table-->
+ <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h1>Pharmacist Portal</h1>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-7">
+
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search data">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>NID</th>
+                                    <th>Name</th>
+                                    <th>Gender</th>
+                                    <th>Medicine</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $con = mysqli_connect("localhost","root","","biometricattendace");
+
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM users WHERE CONCAT(serialnumber,username,gender,medicine) LIKE '%$filtervalues%' ";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['serialnumber']; ?></td>
+                                                    <td><?= $items['username']; ?></td>
+                                                    <td><?= $items['gender']; ?></td>
+                                                    <td><?= $items['medicine']; ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">No Record Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</main>
+</body>
+</html>
